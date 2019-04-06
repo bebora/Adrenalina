adeb36e9559a391f8861aebcb0db03a48e81b246
klaa97 <10521251@polimi.it>
2019-04-06 23:15:11 +0200

    Add tests for Board related methods

---
+++ src/test/java/model/BoardCreatorTest.java
@@ -0,0 +1,30 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardCreatorTest {
+
+    @Test
+    void parseBoard() {
+        Board test = BoardCreator.parseBoard("boards/board1.btlb",8);
+        // Test skull setter
+        assertEquals(test.getSkulls(),8);
+
+        // Test Color parser
+        assertEquals(test.getTiles().get(0).get(0).getRoom(), Color.RED);
+        assertEquals(test.getTiles().get(2).get(2).getRoom(), Color.WHITE);
+
+        //Test positions parsing and setting
+        assertEquals(test.getTiles().get(1).get(2).getPosx(), 2);
+
+        // Test empty spaces in map
+        assertEquals(test.getTiles().get(0).get(3), null);
+
+        // Test spawn points as capital letters
+        assertEquals(test.getTiles().get(0).get(2).isSpawn(), Boolean.TRUE);
+        assertEquals(test.getTiles().get(0).get(1).isSpawn(), Boolean.FALSE);
+    }
+}
--- src/test/java/model/BoardTest.java
+++ src/test/java/model/BoardTest.java
@@ -1,23 +1,77 @@
 package model;
+import java.util.*;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
-
+    Board test;
     @BeforeEach
-    void setUp() {
-
-    }
-
-    @AfterEach
-    void tearDown() {
+    void Before() {
+        test = BoardCreator.parseBoard("boards/board1.btlb",8);
     }
 
     @Test
     void isLinked() {
+        // Test bidirectionaly, given the override of equals for Door Class
+        assertEquals(test.isLinked(test.getTile(0,0), test.getTile(0,1)), true);
+        assertEquals(test.isLinked(test.getTile(0,1), test.getTile(0,0)), true);
+        assertEquals(test.isLinked(test.getTile(1,1), test.getTile(2,1)), true);
+        assertEquals(test.isLinked(test.getTile(1,1), test.getTile(2,1)), true);
+        assertEquals(test.isLinked(test.getTile(1,0), test.getTile(2,1)), false);
+        assertEquals(test.isLinked(test.getTile(1,0), test.getTile(1,1)), false);
+
+
+
+        // Test same rooms and close tiles
+        assertEquals(test.isLinked(test.getTile(1,1), test.getTile(1,2)), true);
+        assertEquals(test.isLinked(test.getTile(2,2), test.getTile(2,3)), false);
+    }
+
+    @Test
+    void visible() {
+        Set<Tile> visibleTiles= test.visibleTiles(test.getTile(1,1));
+        Set<Tile> assertTiles = new HashSet<>();
+        assertTiles.add(test.getTile(1,2));
+        assertTiles.add(test.getTile(0,1));
+        assertTiles.add(test.getTile(0,2));
+        assertTiles.add(test.getTile(1,1));
+        assertTiles.add(test.getTile(2,2));
+        assertTiles.add(test.getTile(2,1));
+        assertTiles.add(test.getTile(2,0));
+
+
+        // Test if all and only the assertTiles are the visibleTiles
+        assertTrue(assertTiles.containsAll(visibleTiles));
+        assertTrue(assertTiles.size() == visibleTiles.size());
+    }
+
+    @Test
+    void reachable() {
+        Set<Tile> reachableTiles = test.reachable(test.getTile(1,1), 2, 2);
+
+        Set<Tile> assertTiles = new HashSet<>();
+        assertTiles.add(test.getTile(0,2));
+        assertTiles.add(test.getTile(0,0));
+        assertTiles.add(test.getTile(1,3));
+        assertTiles.add(test.getTile(2,0));
+        assertTiles.add(test.getTile(2,2));
+        // Test if all ond only the assertTiles are the reachableTiles
+        assertTrue(assertTiles.containsAll(reachableTiles));
+        assertTrue(assertTiles.size() == reachableTiles.size());
+
+
+        reachableTiles = test.reachable(test.getTile(1,1), 0, 1);
+        assertTiles = new HashSet<>();
+        assertTiles.add(test.getTile(0,1));
+        assertTiles.add(test.getTile(1,2));
+        assertTiles.add(test.getTile(2,1));
+        assertTiles.add(test.getTile(1,1));
+
+        // Test if all ond only the assertTiles are the reachableTiles
+        assertTrue(assertTiles.containsAll(reachableTiles));
+        assertTrue(assertTiles.size() == reachableTiles.size());
     }
 }
