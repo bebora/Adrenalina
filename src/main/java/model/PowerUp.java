f80089fc15bbe4ba2e478fb60a256f326249c69e
klaa97 <10521251@polimi.it>
2019-04-06 14:33:04 +0200

    Add builder pattern

--- src/main/java/model/PowerUp.java
+++ src/main/java/model/PowerUp.java
@@ -13,7 +13,9 @@ public class PowerUp {
 	 */
 	private Moment applicability;
 
-
+	/**
+	 * Player saved in the PowerUp temporarily in case of DAMAGING or DAMAGED status
+	 */
 	private Player player;
 
 	/**
@@ -21,8 +23,47 @@ public class PowerUp {
 	 */
 	private Effect effect;
 
-
+	/**
+	 * Name of the PowerUp
+	 */
 	private String name;
+
+	public static class Builder {
+		private Ammo discardAward;
+		private Moment applicability;
+		private Effect effect;
+		private String name;
+
+		public Builder setDiscardAward(Ammo discardAward) {
+			this.discardAward = discardAward;
+			return this;
+		}
+
+		public Builder setApplicability(Moment applicability) {
+			this.applicability = applicability;
+			return this;
+		}
+
+		public Builder setEffect(Effect effect) {
+			this.effect = effect;
+			return this;
+		}
+
+		public Builder setName(String name) {
+			this.name = name;
+			return this;
+		}
+		public  PowerUp build(){
+			return new PowerUp(this);
+		}
+	}
+
+	public PowerUp(Builder builder) {
+		this.discardAward = builder.discardAward;
+		this.applicability = builder.applicability;
+		this.effect = builder.effect;
+		this.name = builder.name;
+	}
 
 
 
