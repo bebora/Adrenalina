3f24ab0681810ef89c538bde753d7d1336fb80e4
klaa97 <10521251@polimi.it>
2019-04-06 10:25:31 +0200

    Add JavaDoc and methods scheme

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -10,27 +10,34 @@ public class Player {
 	}
 
 	/**
-	 * Real position of the player
+	 * Real position of the Player
 	 */
 	private Tile tile;
 
-
+	/**
+	 * Unique identifier of the Player
+	 */
 	private int id;
 
 	/**
 	 * Ordered list of damage tokens received, represented by players who gave them
 	 */
-	private ArrayList<Player> damages;
+	private List<Player> damages;
 
 	/**
 	 * Ordered list of marks received, represented by players who gave them
 	 */
-	private ArrayList<Player> marks;
+	private List<Player> marks;
 
+	/**
+	 * Owned ammos by the Player; there can't be more than three for each color
+	 * No Ammo.POWERUP is allowed into the Set
+	 */
+	private Set<Ammo> ammos;
 
-	private ArrayList<Ammo> ammos;
-
-
+	/**
+	 * If True, the Player is the first player.
+	 */
 	private Boolean firstPlayer;
 
 	/**
@@ -48,10 +55,14 @@ public class Player {
 	 */
 	private ArrayList<Integer> rewardPoints;
 
-
+	/**
+	 * Loaded and unloaded weapons owned by the Player
+	 */
 	private ArrayList<Weapon> weapons;
 
-
+	/**
+	 * How many kills the player did in the current turn
+	 */
 	private int kills;
 
 	/**
@@ -59,10 +70,15 @@ public class Player {
 	 */
 	private int actionCount;
 
-
+	/**
+	 * Represent the status of the Player; when transitioning and at the end of the turn,
+	 * the Controller will respawn the player accordingly to the rules.
+	 */
 	private ThreeState alive;
 
-
+	/**
+	 * PowerUps cards owned by the Player
+	 */
 	private ArrayList<PowerUp> powerUps;
 
 	/**
@@ -80,11 +96,25 @@ public class Player {
 	 */
 	private Boolean turnEnd;
 
+	/**
+	 * Number of time that the player has been killed before FinalFrenzy
+	 */
+	private int trackSkulls;
 
-	public void convertMarks(int idPlayer) {
-}
+	public void convertMarks(Player player) {
+		//TODO
+	}
+	public void shoot(Player shooter, int damage, int marks) {
+		//TODO
+	}
+	public void updateActions() {
+		//TODO
+		// Create Resources file for according action and parse those according to the number of skulls remaining
+	}
 
 	public void reload(Weapon weapon) {
-}
-
+		//TODO
+		//Complete the method, create an exception according to the examples and make it throw that
+		//If for example the player doesn't have enough $$ to reload.
+	}
 }
