ec880312e19f4f88e08c4c689abaf0b611391133
klaa97 <10521251@polimi.it>
2019-04-21 12:43:37 +0200

    Add update messages skeletrum

---
+++ src/main/java/it/polimi/se2019/model/UpdateMessage/Update.java
@@ -0,0 +1,5 @@
+package it.polimi.se2019.model.UpdateMessage;
+
+public interface Update {
+    //TODO details of what to update; implement the command or visitor pattern
+}
--- src/main/java/it/polimi/se2019/model/UpdateMessage/UpdateWrapper.java
+++ src/main/java/it/polimi/se2019/model/UpdateMessage/UpdateWrapper.java
@@ -2,4 +2,5 @@ package it.polimi.se2019.model.UpdateMessage;
 
 public class UpdateWrapper {
     //TODO wraps the object responsible for the updates needed to the views, created by the model, updates VirtualView through Observer
+
 }
