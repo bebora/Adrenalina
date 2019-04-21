4d8d7ed3babdbc094364503eb9263a24541abb75
klaa97 <10521251@polimi.it>
2019-04-22 00:11:00 +0200

    Fix comments and bad refactoring

--- src/main/java/it/polimi/se2019/view/VirtualView.java
+++ src/main/java/it/polimi/se2019/view/VirtualView.java
@@ -1,10 +1,9 @@
 package it.polimi.se2019.view;
 
-import it.polimi.se2019.controller.Observer;
 import it.polimi.se2019.controller.EventVisitable;
 
-public class VirtualView extends View implements Observer {
-    //TODO it.polimi.se2019.controller.Observer class, the it.polimi.se2019.controller update the it.polimi.se2019.model, the it.polimi.se2019.model update the VirtualView passing a message with info on update needed, and the virtualview using the server interface updates the corresponding views
+public class VirtualView extends View  {
+    //TODO Observer class, the controller update the model, the model update the VirtualView passing a message with info on update needed, and the virtualview using the server interface updates the corresponding views
 
     /**
      * //TODO used by model to update, passing updates to do to the virtualview
