c664bb684049d1d274129c8713169f7058ba4c3d
TheRealFake <alfarigolix@gmail.com>
2019-04-25 12:07:20 +0200

    Reworked getUsableEffect using streams

--- src/main/java/it/polimi/se2019/controller/WeaponController.java
+++ src/main/java/it/polimi/se2019/controller/WeaponController.java
@@ -6,12 +6,13 @@ import it.polimi.se2019.model.Player;
 import it.polimi.se2019.model.cards.Effect;
 import it.polimi.se2019.model.cards.Weapon;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WeaponController extends Thread {
     private int curEffect;
-    private int effectIndex;
+    private int lastUsedIndex;
     private Weapon weapon;
     private Match match;
     private Player curPlayer;
@@ -20,28 +21,39 @@ public class WeaponController extends Thread {
         this.match = match;
         this.weapon = weapon;
         this.curEffect = 1;
-        this.effectIndex = -1;
+        this.lastUsedIndex = -1;
         this.curPlayer = match.getPlayers().get(match.getCurrentPlayer());
     }
     public List<String> getUsableEffects(){
-        List<String> usableEffects = new ArrayList<>();
         List<Effect> allEffects = weapon.getEffects();
-        String curName;
-        for(Effect e: allEffects) {
-            if (curPlayer.checkForAmmos(e.getCost())) {
-                if (e.getAbsolutePriority() != 0) {
-                    if (e.getAbsolutePriority() == curEffect)
-                        usableEffects.add(e.getName());
-                } else {
-                    for (Integer i : e.getRelativePriority()) {
-                        curName = allEffects.get(Math.abs(i) - 1).getName();
-                        if(usableEffects.contains(curName) && i < 0)
-                                usableEffects.add(e.getName());
-                    }
-                }
-            }
-        }
-        return usableEffects;
+
+        Stream<Effect> possiblyUsableEffects = allEffects.stream()
+                .filter(effect -> !effect.getActivated())
+                .filter(effect -> curPlayer.checkForAmmos(effect.getCost()));
+
+        List<Integer> notUsedIndexes = allEffects.stream()
+                .filter(effect -> !effect.getActivated())
+                .map(allEffects::indexOf)
+                .collect(Collectors.toList());
+
+        Stream<String> checkAbsolute = possiblyUsableEffects
+                .filter(effect -> effect.getAbsolutePriority() != 0)
+                .filter(effect -> effect.getAbsolutePriority() == curEffect)
+                .map(Effect::getName);
+
+        Stream<String> checkRelativeAfter = possiblyUsableEffects
+                .filter(effect-> effect.getAbsolutePriority() == 0)
+                .filter(effect-> effect.getRelativePriority().contains(lastUsedIndex))
+                .map(Effect::getName);
+
+        Stream<String> checkRelativeBefore = possiblyUsableEffects
+                .filter(effect -> effect.getAbsolutePriority() == 0)
+                .filter(effect -> effect.getRelativePriority().stream().filter(i -> i<0).anyMatch(i->notUsedIndexes.contains(-i)))
+                .map(Effect::getName);
+
+        Stream<String> checkRelative = Stream.concat(checkRelativeAfter,checkRelativeBefore).distinct();
+
+        return Stream.concat(checkAbsolute,checkRelative).collect(Collectors.toList());
     }
     void update(EventWrapper e) {
         //TODO will update when the data will be avaiable, notifying the process that is waiting
