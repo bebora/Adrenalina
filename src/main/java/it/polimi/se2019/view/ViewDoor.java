78f7369c118138d07a135611123d6ae84f69b9b1
TheRealFake <alfarigolix@gmail.com>
2019-05-12 17:12:27 +0200

    Add isLinked, add cabDistance,fix door position

--- src/main/java/it/polimi/se2019/view/ViewDoor.java
+++ src/main/java/it/polimi/se2019/view/ViewDoor.java
@@ -9,4 +9,19 @@ public class ViewDoor {
         this.tile1 = new ViewTile(door.getTile1());
         this.tile2 = new ViewTile(door.getTile2());
     }
+
+    public ViewDoor(ViewTile tile1, ViewTile tile2){
+        this.tile1 = tile1;
+        this.tile2 = tile2;
+    }
+
+    @Override
+    public boolean equals(Object p) {
+        boolean returnValue = false;
+        if (p instanceof ViewDoor) {
+            ViewDoor door = (ViewDoor) p;
+            returnValue = (door.tile1.equals(this.tile1) && door.tile2.equals(this.tile2)) || (door.tile1.equals(this.tile2) && door.tile2.equals(this.tile1));
+        }
+        return returnValue;
+    }
 }
