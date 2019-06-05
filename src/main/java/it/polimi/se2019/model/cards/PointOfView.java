74b64dfa4c1909e2be5dc86e798d751d6a030bb7
bebora <simone3.rizzo@mail.polimi.it>
2019-06-05 11:25:24 +0200

    Add thor and tractorBeam

--- src/main/java/it/polimi/se2019/model/cards/PointOfView.java
+++ src/main/java/it/polimi/se2019/model/cards/PointOfView.java
@@ -3,7 +3,7 @@ package it.polimi.se2019.model.cards;
 /**
  * OWN: physical position of the attacking player
  * PERSPECTIVE: virtual position of the attacking player
- * LASTPLAYER: position of the last attacked player
+ * LASTPLAYER: position of the last attacked player in TargetList
  * TARGET: position of selected target player
  */
 public enum PointOfView {
