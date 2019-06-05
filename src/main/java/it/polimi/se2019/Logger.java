25553d751711b5e33518364a8b2766843cb312e3
bebora <simone3.rizzo@mail.polimi.it>
2019-06-05 19:58:32 +0200

    Log to both file and stdout

--- src/main/java/it/polimi/se2019/Logger.java
+++ src/main/java/it/polimi/se2019/Logger.java
@@ -1,5 +1,12 @@
 package it.polimi.se2019;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public final class Logger {
 
 
@@ -8,23 +15,50 @@ public final class Logger {
     }
 
     private static final String HOME = System.getProperty("user.home");
-    private static final String LOG = String.format("%s/adrenalina.log", HOME);
-
+    private static String LOG = String.format("%s/adrenalina.log", HOME);
+    private static boolean logToFile;
     private static boolean debugMode;
-    //TODO log somewhere (cli/file)
+    private static BufferedWriter bw;
     public static void log(Priority priority, String toLog) {
+        DateFormat dateFormat = new SimpleDateFormat("[yyyy/MM/dd - HH:mm:ss:SSS]");
+        Date now = new Date();
+        String logWithTime = dateFormat.format(now);
         switch (priority) {
             case DEBUG:
-                toLog = "[*] DEBUG: " + toLog;
+                logWithTime += " DEBUG: " + toLog;
                 break;
             case ERROR:
-                toLog = "[*] ERROR: " + toLog;
+                logWithTime += " ERROR: " + toLog;
                 break;
             case WARNING:
-                toLog = "[*] WARNING: " + toLog;
+                logWithTime += " WARNING: " + toLog;
                 break;
         }
-        //TODO LOG TO RELATIVE FILE
-        System.out.println(toLog);
+        if (logToFile) {
+            try {
+                bw.write(logWithTime+"\n");
+                bw.flush();
+            }
+            catch (IOException e) {
+                logToFile = false;
+                System.out.println("Could not log to file");
+            }
+
+        }
+        System.out.println(logWithTime);
+    }
+
+    public static void setLogFileSuffix(String suffix) {
+        if (!logToFile) {
+            LOG += suffix;
+            System.out.println("Logging to: "+LOG);
+            try {
+                bw = new BufferedWriter(new FileWriter(LOG, true));
+                logToFile = true;
+            }
+            catch (IOException e) {
+                System.out.println("Could not log to file");
+            }
+        }
     }
 }
